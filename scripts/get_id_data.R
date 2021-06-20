edce57521314f7ced183284bfff932c44da4d717
jwwood2020 <78509344+jwwood2020@users.noreply.github.com>
2021-06-20 15:59:16 +0100

    read in raw data

---
+++ scripts/get_id_data.R
@@ -0,0 +1,48 @@
+
+# About -------------------------------------------------------------------
+
+# This script reads in the "Datazone2011lookup.csv" file.
+
+# This file contains identifiers and lookups for the various geographic regions,
+# health boards etc. 
+#
+# Nothing has been done to this data other than clean names. If identifiers,
+# lookups, etc are required for other analysis then this csv should be used.
+
+
+# Load libraries ----------------------------------------------------------
+
+library(tidyverse)
+library(janitor)
+
+
+# Read in .csv ------------------------------------------------------------
+
+id_lookups <- read_csv("data/raw/Datazone2011lookup.csv") %>% 
+  clean_names()
+
+
+# Write .csv  -------------------------------------------------------------
+
+write_csv(id_lookups, "data/clean/id_lookups.csv")
+
+
+
+# Get health board names --------------------------------------------------
+
+health_board_id <- id_lookups %>% 
+  select(hb_code, hb_name) %>% 
+  distinct(hb_code, .keep_all = TRUE)
+
+write_csv(health_board_id, "data/clean/health_board_id.csv")
+
+
+
+# Get Council names -------------------------------------------------------
+
+council_id <- id_lookups %>% 
+  select(la_code, la_name) %>% 
+  distinct(la_code, .keep_all = TRUE)
+
+write_csv(council_id, "data/clean/council_id.csv")
+  
