0aee404692e61f866127c24b8897008163b147ad
jwwood2020 <78509344+jwwood2020@users.noreply.github.com>
2021-06-21 21:46:04 +0100

    more work on data manipulation script & initial EDA

--- scripts/data_manipulation.R
+++ scripts/data_manipulation.R
@@ -12,7 +12,7 @@ library(tidyverse)
 # Two sets of data: 
 #  1. Hospital activity related to strokes
 #  2. Stroke mortality data
-# Both datasets are produced at Health Board (hb) and Council Area (ca)
+# Both datasets are produced at Health Board (hb) and Council Area (ca) level
 # Columns ending 'qf' are codes relating to data quality and can be removed
 
 activity_hb_raw <- read_csv("data/raw/stroke_activitybyhb.csv") %>% 
@@ -53,88 +53,108 @@ population_proj_ca_raw <- read_csv("data/raw/population_proj_ca.csv") %>%
 
 
 # Read Health Board/Council id lookups ------------------------------------
-hb_id <- read_csv("data/clean/health_board_id.csv")
-ca_id <- read_csv("data/clean/council_id.csv")
-id_lookups <- read_csv("data/clean/id_lookups.csv")
+health_board_id <- read_csv("data/raw/healthboard_id.csv") %>% 
+  clean_names() %>% 
+  select(hb, hb_name)
 
-
-# Get HB names for councils -----------------------------------------------
-hb_ca_id <- id_lookups %>% 
-  select("la_code", "hb_name") %>% 
-  distinct(la_code, hb_name)
+council_id <- read_csv("data/raw/council_id.csv") %>% 
+  clean_names() %>% 
+  select(ca, ca_name, hb_name) 
 
 
 # Wrangle activity data ---------------------------------------------------
 activity_hb <- activity_hb_raw %>% 
-  left_join(hb_id, by = c("hbr" = "hb_code")) %>% # get Health Board name
+  left_join(health_board_id, by = c("hbr" = "hb")) %>% # get Health Board name
   mutate(year = str_sub(financial_year, 1, 4),
-         hb_name = if_else(hbr == "S92000003", "Scotland", hb_name) 
-  ) %>% 
+         hb_name = if_else(hbr == "S92000003", "Scotland", hb_name),
+         number_of_discharges = coalesce(number_of_discharges, 0),
+         crude_rate = coalesce(crude_rate, 0)
+         ) %>% 
   select(-financial_year) %>% 
   relocate(year)
 
 activity_ca <- activity_ca_raw %>% 
-  left_join(ca_id, by = c("ca" = "la_code")) %>%  # get Council name
-  left_join(hb_ca_id, by = c("ca" = "la_code")) %>% # get Health Board name
+  left_join(council_id, by = "ca") %>%  # get Council name
   mutate(year = str_sub(financial_year, 1, 4),
-         la_name = if_else(ca == "S92000003", "Scotland", la_name),
-         hb_name = if_else(ca == "S92000003", "Scotland", hb_name)
-  ) %>% 
+         ca_name = if_else(ca == "S92000003", "Scotland", ca_name),
+         hb_name = if_else(ca == "S92000003", "Scotland", hb_name),
+         number_of_discharges = coalesce(number_of_discharges, 0),
+         crude_rate = coalesce(crude_rate, 0)
+         ) %>% 
   select(-financial_year) %>% 
   relocate(year)
 
 
 # Wrangle mortality data --------------------------------------------------
 mortality_hb <- mortality_hb_raw %>% 
-  left_join(hb_id, by = c("hbr" = "hb_code")) %>% # get Health Board name
-  mutate(hb_name = if_else(hbr == "S92000003", "Scotland", hb_name))
+  left_join(health_board_id, by = c("hbr" = "hb")) %>% # get Health Board name
+  mutate(hb_name = if_else(hbr == "S92000003", "Scotland", hb_name),
+         number_of_deaths = coalesce(number_of_deaths, 0),
+         crude_rate = coalesce(crude_rate, 0)
+         )
 
 mortality_ca <- mortalilty_ca_raw %>% 
-  left_join(ca_id, by = c("ca" = "la_code")) %>%  # get Council name
-  left_join(hb_ca_id, by = c("ca" = "la_code")) %>% # get Health Board name
-  mutate(la_name = if_else(ca == "S92000003", "Scotland", la_name),
-         hb_name = if_else(ca == "S92000003", "Scotland", hb_name)
-  )
-
+  left_join(council_id, by = "ca") %>%  # get Council name
+  mutate(ca_name = if_else(ca == "S92000003", "Scotland", ca_name),
+         hb_name = if_else(ca == "S92000003", "Scotland", hb_name),
+         number_of_deaths = coalesce(number_of_deaths, 0),
+         crude_rate = coalesce(crude_rate, 0)
+         )
+  
 
 # Wrangle population estimates --------------------------------------------
 population_hb <- population_hb_raw %>%
-  pivot_longer(cols = c("age0":"age90plus"),
+  pivot_longer(cols = c("all_ages":"age90plus"),
                names_to = "age",
-               values_to = "count") %>% 
-  left_join(hb_id, by = c("hb" = "hb_code")) %>% # get Health Board name
-  mutate(age = str_sub(age, 4, -1),
-         hb_name = if_else(hb == "S92000003", "Scotland", hb_name))
+               values_to = "count"
+               ) %>% 
+  left_join(health_board_id, by = c("hb" = "hb")) %>% # get Health Board name
+  mutate(age = if_else(str_sub(age, 1, 3) == "age", 
+                       str_sub(age, 4, -1),
+                       age),
+         hb_name = if_else(hb == "S92000003", "Scotland", hb_name),
+         ) %>% 
+  select(-id)
 
 population_ca <- population_ca_raw %>%
-  pivot_longer(cols = c("age0":"age90plus"),
+  pivot_longer(cols = c("all_ages":"age90plus"),
                names_to = "age",
-               values_to = "count") %>% 
-  left_join(ca_id, by = c("ca" = "la_code")) %>%  # get Council name
-  left_join(hb_ca_id, by = c("ca" = "la_code")) %>% # get Health Board name
-  mutate(age = str_sub(age, 4, -1),
-         la_name = if_else(ca == "S92000003", "Scotland", la_name),
-         hb_name = if_else(ca == "S92000003", "Scotland", hb_name))
+               values_to = "count"
+               ) %>% 
+  left_join(council_id, by = "ca") %>%  # get Council name
+  mutate(age = if_else(str_sub(age, 1, 3) == "age",
+                       str_sub(age, 4, -1),
+                       age),
+         ca_name = if_else(ca == "S92000003", "Scotland", ca_name),
+         hb_name = if_else(ca == "S92000003", "Scotland", hb_name)) %>% 
+  select(-id)
 
 
 # Wrangle population projections ------------------------------------------
 population_proj_hb <- population_proj_hb_raw %>%
-  pivot_longer(cols = c("age0":"age90plus"),
+  pivot_longer(cols = c("all_ages":"age90plus"),
                names_to = "age",
-               values_to = "count") %>% 
-  left_join(hb_id, by = c("hb" = "hb_code")) %>% # get Health Board name
-  mutate(age = str_sub(age, 4, -1),
-         hb_name = if_else(hb == "S92000003", "Scotland", hb_name))
+               values_to = "count"
+               ) %>% 
+  left_join(health_board_id, by = c("hb" = "hb")) %>% # get Health Board name
+  mutate(age = if_else(str_sub(age, 1, 3) == "age",
+                       str_sub(age, 4, -1),
+                       age),
+         hb_name = if_else(hb == "S92000003", "Scotland", hb_name)) %>% 
+  select(-id)
 
 population_proj_ca <- population_proj_ca_raw %>%
-  pivot_longer(cols = c("age0":"age90plus"),
+  pivot_longer(cols = c("all_ages":"age90plus"),
                names_to = "age",
-               values_to = "count") %>% 
-  left_join(ca_id, by = c("ca" = "la_code")) %>%  # get Council name
-  left_join(hb_ca_id, by = c("ca" = "la_code")) %>% # get Health Board name
-  mutate(age = str_sub(age, 4, -1),
-         la_name = if_else(ca == "S92000003", "Scotland", la_name),
-         hb_name = if_else(ca == "S92000003", "Scotland", hb_name))
+               values_to = "count"
+               ) %>% 
+  left_join(council_id, by = "ca") %>%  # get Council name
+  mutate(age = if_else(str_sub(age, 1, 3) == "age",
+                       str_sub(age, 4, -1),
+                       age),
+         ca_name = if_else(ca == "S92000003", "Scotland", ca_name),
+         hb_name = if_else(ca == "S92000003", "Scotland", hb_name)) %>% 
+  select(-id)
 
 
 # Write .csv  -------------------------------------------------------------
--- scripts/get_id_data.R
+++ scripts/get_id_data.R
@@ -11,35 +11,26 @@
 
 
 # Load libraries ----------------------------------------------------------
-
 library(tidyverse)
 library(janitor)
 
 
 # Read in .csv ------------------------------------------------------------
-
 id_lookups <- read_csv("data/raw/Datazone2011lookup.csv") %>% 
   clean_names()
-
-
-# Write .csv  -------------------------------------------------------------
 
 write_csv(id_lookups, "data/clean/id_lookups.csv")
 
 
-
 # Get health board names --------------------------------------------------
+health_board_id <- read_csv("data/raw/healthboard_id.csv") %>% 
+  clean_names()
 
-health_board_id <- id_lookups %>% 
-  select(hb_code, hb_name) %>% 
-  distinct(hb_code, .keep_all = TRUE)
 
 write_csv(health_board_id, "data/clean/health_board_id.csv")
 
 
-
 # Get Council names -------------------------------------------------------
-
 council_id <- id_lookups %>% 
   select(la_code, la_name) %>% 
   distinct(la_code, .keep_all = TRUE)
